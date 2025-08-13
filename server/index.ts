68e484c844b8ad1bbaf1ca2c1119b00bae195868
Vedant-code <vedant.dhote20@pccoepune.org>
2025-08-14 00:02:44 +0530

    corrected build errors

--- server/index.ts
+++ server/index.ts
@@ -60,12 +60,22 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
-  });
+
+  // Skip server listen in production on Vercel
+  if (process.env.VERCEL !== "1") {
+    const port = parseInt(process.env.PORT || "5000", 10);
+    const host =
+      process.env.NODE_ENV === "production" ? "0.0.0.0" : "localhost";
+
+    server.listen(
+      {
+        port,
+        host,
+        ...(process.platform !== "win32" ? { reusePort: true } : {}),
+      },
+      () => {
+        log(`serving on port ${port}`);
+      }
+    );
+  }
 })();
